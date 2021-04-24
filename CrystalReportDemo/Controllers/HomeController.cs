b2b99e4d83a3e4be54da3cf0ef9946b1d6959b6b
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-04-24 17:33:33 +0600

    CrystalReport in excel file

--- CrystalReportDemo/Controllers/HomeController.cs
+++ CrystalReportDemo/Controllers/HomeController.cs
@@ -40,6 +40,34 @@ namespace CrystalReportDemo.Controllers
             }
         }
 
+
+        public ActionResult TestWithModel()
+        {
+            var list = databaseContext.Employees.ToList();
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/CrystalReports"), "TestWithModel.rpt"));
+            rd.SetDataSource(list);
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.ExcelRecord);
+                stream.Seek(0, SeekOrigin.Begin);
+                //return File(stream, "application/pdf", "Employee_List_Model.pdf");//"application/excel
+                //return File(stream, "application/excel", "Employee_List_Model.xlsx");
+                //return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "eNtsaRegistrationForm.xls");
+                //return File(stream, "application/vnd.ms-excel", "eNtsaRegistrationForm.xls");
+                //return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "eNtsaRegistrationForm.xlsx");
+                return File(stream, "application/vnd.ms-excel", "eNtsaRegistrationForm..xls");
+                 
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
